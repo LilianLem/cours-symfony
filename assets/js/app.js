1455e5b10a9fd9fc046c6036793dd42907d42595
LilianLem <54894780+LilianLem@users.noreply.github.com>
2020-06-18 14:29:01 +0200

    Revert "Added Leaflet (not working)"
    This reverts commit 869b985eacaa091b972d9c16d08919c569261e86.

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,6 @@ import '../css/app.css';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 
+const $ = require('jquery');
+
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
