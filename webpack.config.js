5553740ab08efa2a75baa10b4ef138e4d30123cb
LilianLem <54894780+LilianLem@users.noreply.github.com>
2020-06-18 14:28:59 +0200

    Revert "Essai avec JS Leaflet en local"
    This reverts commit 85aee5d63d42a26e52e16d54113156b0767e48c7.

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('leaflet', './assets/js/leaflet.js')
-    .addEntry('leafletMap', './assets/js/leaflet-map.js')
+    //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
